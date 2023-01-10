692acb5a8171d515bc816c158cb87c81bfeb50a6
MarcusTL12 <marcuspaafjellet@gmail.com>
2023-01-10 14:48:24 +0000

    c

--- pre-assignment/solution.jl
+++ pre-assignment/solution.jl
@@ -28,7 +28,7 @@ function simulate_first_return(dist, max_steps)
         x = nx
     end
 
-    max_steps
+    max_steps + 1
 end
 
 function simulate_n_returns(dist, max_steps, n)
@@ -65,7 +65,7 @@ function run_a()
     max_steps = 1000
     n_simulations = 100_000_000
 
-    ts = simulate_n_returns(randn, max_steps + 1, n_simulations)
+    ts = simulate_n_returns(randn, max_steps, n_simulations)
     cs = make_counts(ts, max_steps)
     ps = make_prob_dist(cs, n_simulations)
 
@@ -84,7 +84,7 @@ function find_alpha(ps, p0=[1.0, 1.0])
 end
 
 function run_b(max_steps, n_simulations)
-    ts = simulate_n_returns(randn, max_steps + 1, n_simulations)
+    ts = simulate_n_returns(randn, max_steps, n_simulations)
     cs = make_counts(ts, max_steps)
     ps = make_prob_dist(cs, n_simulations)
 
@@ -98,7 +98,7 @@ end
 function improve_b((p, cs), n_simulations)
     max_steps = length(cs)
 
-    ts = simulate_n_returns(randn, max_steps + 1, n_simulations)
+    ts = simulate_n_returns(randn, max_steps, n_simulations)
     make_counts(ts, max_steps, cs)
     ps = make_prob_dist(cs, n_simulations)
 
@@ -107,4 +107,58 @@ function improve_b((p, cs), n_simulations)
     println("α = ", p[2] ± e[2])
 
     p, cs
+end
+
+function simulate_first_crossing(dist, a, max_steps)
+    x = 0.0
+
+    for t in 0:max_steps
+        dx = dist()
+        x += dx
+        if x > a
+            return t
+        end
+    end
+
+    max_steps + 1
+end
+
+function simulate_n_crossings(dist, a, max_steps, n)
+    ts = zeros(Int, n)
+
+    Threads.@threads for i in 1:n
+        ts[i] = simulate_first_crossing(dist, a, max_steps)
+    end
+
+    ts
+end
+
+function run_c()
+    max_steps = 1000
+    n_simulations = 100_000_000
+    a = 10
+
+    dist = () -> rand() * 2 - 1
+
+    ts = simulate_n_crossings(dist, a, max_steps, n_simulations)
+    cs = make_counts(ts, max_steps)
+    ps = make_prob_dist(cs, n_simulations)
+
+    t_plot = 1:1000
+
+    plot((@view ps[t_plot]); label="Simulated")
+
+    model(t, (c, a, b)) = @. c * t^(-a) * exp(-b / t)
+
+    p0 = [24.0, 1.7, 200.0]
+
+    fit = curve_fit(
+        model, collect(eachindex(ps)), ps, p0;
+        autodiff=:forwarddiff
+    )
+
+    param = fit.param .± stderror(fit)
+    @show param
+
+    plot!(t_plot, model(t_plot, fit.param); label="Fit")
 end
