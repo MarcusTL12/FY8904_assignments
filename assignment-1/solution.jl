6387a6d0c5dde348395077b46bc2748b4546b0ac
MarcusTL12 <marcuspaafjellet@gmail.com>
2023-01-16 16:53:35 +0000

    more anim

--- assignment-1/solution.jl
+++ assignment-1/solution.jl
@@ -10,13 +10,24 @@ function test_crater()
 
     pq = init_collisions(ps, vs, rs)
 
-    (i, j), t = dequeue_pair!(pq)
+    display(pq)
 
-    img_i = animate_line(ps, vs, rs, 0.0, t, 100, 1, "assignment-1/tmp_anim/", 1000)
+    (i, j), t1 = dequeue_pair!(pq)
 
-    ps .+= vs .* t
+    speed = 300
+
+    img_i = animate_line(ps, vs, rs, 0.0, t1, ceil(Int, speed * t1), 1, "assignment-1/tmp_anim/", 1000)
+
+    ps .+= vs .* t1
 
     do_collision!(i, j, ps, vs, ms, 1.0)
 
-    animate_line(ps, vs, rs, 0.0, 0.05, 30, img_i, "assignment-1/tmp_anim/", 1000; include_first=false)
+    update_collisions!(pq, i, t1, ps, vs, rs)
+    update_collisions!(pq, j, t1, ps, vs, rs)
+
+    (i, j), t2 = dequeue_pair!(pq)
+
+    animate_line(ps, vs, rs, 0.0, t2 - t1, ceil(Int, speed * (t2 - t1)), img_i, "assignment-1/tmp_anim/", 1000; include_first=false)
+
+    pq
 end
