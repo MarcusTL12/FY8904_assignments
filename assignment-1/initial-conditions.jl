55918f06180d17e6178b6f923865b0e29fcb5d93
MarcusTL12 <marcuspaafjellet@gmail.com>
2023-01-16 15:19:11 +0000

    crater initialization

--- assignment-1/initial-conditions.jl
+++ assignment-1/initial-conditions.jl
@@ -19,7 +19,7 @@ function still_grid(x1, x2, y1, y2, nx, ny, r, m)
 end
 
 # r = k * d
-function crater_setup(nx, k, density)
+function crater_setup(nx, k, density, p_r, p_density, p_v)
     r = 1 / (2nx + (nx + 1) / k)
     d = r / k
 
@@ -33,5 +33,20 @@ function crater_setup(nx, k, density)
     y1 = (1 - h) + x1
     y2 = x2
 
-    still_grid(x1, x2, y1, y2, nx, ny, r, 1.0)
+    area = π * r^2
+    m = area * density
+
+    ps, vs, rs, ms = still_grid(x1, x2, y1, y2, nx, ny, r, m)
+
+    px = 0.5
+    py = p_r + 0.01
+
+    θ = randn() * 0.1 - π / 2
+    pvx = p_v * cos(θ)
+    pvy = p_v * sin(θ)
+
+    p_area = π * p_r^2
+    p_m = p_area * p_density
+
+    [ps; px py], [vs; pvx pvy], [rs; p_r], [ms; p_m]
 end
