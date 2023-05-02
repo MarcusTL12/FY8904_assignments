430a5a53605ae4ce8fe28e37aaa9261f8d9cd9fe
MarcusTL12 <marcuspaafjellet@gmail.com>
2023-05-02 18:01:08 +0200

    Seems to be working

---
+++ exam/mc2d.jl
@@ -0,0 +1,96 @@
+
+# Simple utility functions to rotate a vector 90 degrees left or right
+rotl((x, y),) = (-y, x)
+rotr((x, y),) = (y, -x)
+
+# This function will check if moving monomer number i is possible, and return
+# the destination coordinate if it is.
+function is_transition_possible(chain, coord_map, i)
+    if i == 1 || i == length(chain)
+        dest1, dest2 = if i == 1
+            chain[2] .+ rotl(chain[1] .- chain[2]),
+            chain[2] .+ rotr(chain[1] .- chain[2])
+        else
+            chain[end-1] .+ rotl(chain[end] .- chain[end-1]),
+            chain[end-1] .+ rotr(chain[end] .- chain[end-1])
+        end
+
+        possible1 = !haskey(coord_map, dest1)
+        possible2 = !haskey(coord_map, dest2)
+
+        if possible1 && possible2
+            # If both are possible, pick one at random
+            return (true, rand((dest1, dest2),))
+        elseif possible1
+            return (true, dest1)
+        elseif possible2
+            return (true, dest2)
+        end
+    else
+        # Short hand to not have to write out 'chain[...]' too many times
+        m0, m1, m2 = chain[i-1], chain[i], chain[i+1]
+
+        # Semi black magic to check whether monomer i is on a corner
+        # By forming the vector from monomer i - 1 and i + 1, this needs to
+        # have a non-zero x and y component for i to be on a corner.
+        # We check this by multiplying the x and y component and checking
+        # if the result is 0
+        if prod(m2 .- m0) != 0
+            # Observing that the new coordinate of the monomer is obtained from
+            # moving it towards both its bonded monomers.
+            dest_coord = m0 .+ m2 .- m1
+            return (!haskey(coord_map, dest_coord), dest_coord)
+        end
+    end
+
+    (false, (0, 0))
+end
+
+function do_mc_draw!(chain, coord_map, interaction_matrix, monomer_types,
+    prev_energy, temperature)
+
+    # Draw one of the possible transitions at random.
+    i = rand(1:length(chain))
+
+    possible, dest_coord = is_transition_possible(chain, coord_map, i)
+
+    if possible
+        # Save this to revert if rejected
+        prev_coord = chain[i]
+
+        # Update the structure
+        chain[i] = dest_coord
+        delete!(coord_map, prev_coord)
+        coord_map[dest_coord] = i
+
+        new_energy = calculate_energy_direct(
+            interaction_matrix, monomer_types, chain, coord_map
+        )
+
+        ΔE = new_energy - prev_energy
+
+        # Metropolis acceptance probability
+        p_accept = min(1.0, exp(-ΔE / temperature))
+
+        # If rejected, we need to revert the changes to the polymer structure
+        if rand() > p_accept
+            chain[i] = prev_coord
+            delete!(coord_map, dest_coord)
+            coord_map[prev_coord] = i
+            prev_energy
+        else
+            new_energy
+        end
+    else
+        prev_energy
+    end
+end
+
+function do_mc_sweep!(chain, coord_map, interaction_matrix, monomer_types,
+    energy, temperature)
+    for _ in 1:length(chain)
+        energy = do_mc_draw!(chain, coord_map, interaction_matrix,
+            monomer_types, energy, temperature)
+    end
+    energy
+end
--- exam/solution.jl
+++ exam/solution.jl
@@ -3,154 +3,65 @@ import Plots
 using Printf
 
 include("visualization.jl")
-
-# This is for initializing a 2d linear "protein" chain starting at the origin
-# and reaching towards positive x
-# Calling this for n = 4 will return the list
-# [(0, 0), (1, 0), (2, 0), (3, 0)]
-function make_linear_2d_chain(n)
-    [(i, 0) for i in 0:(n-1)]
-end
-
-# This is for initializing a 2d perfectly folded protein starting at the origin
-# and moving upwards 'w' units, then right one, then down another 'w' units.
-# n = 20, and w = 5 gives the following chain:
-# ╔╗╔╗║
-# ║║║║║
-# ║║║║║
-# ║║║║║
-# ║╚╝╚╝
-function make_folded_2d_chain(n, w)
-    coord = (0, 0)
-
-    chain = NTuple{2,Int}[]
-
-    dir = 1
-
-    while length(chain) < n
-        for i in 1:w
-            push!(chain, coord)
-            if i < w
-                coord = coord .+ (0, dir)
-            end
-
-            if length(chain) >= n
-                break
-            end
-        end
-
-        coord = coord .+ (1, 0)
-
-        dir = -dir
-    end
-
-    chain
-end
-
-# This translates the chain into a dictionary (Hashmap) from integer coordinates
-# to the index in the chain. This makes for a both space and time efficient
-# lookup table for whether a given coordinate is occupied, and by which one.
-function make_coord_map(chain)
-    Dict(coord => i for (i, coord) in enumerate(chain))
-end
-
-# This takes in the previous neighbour_list (or an empty one), and updates
-# the list to the given chain. This is to omit the allocation of a new list.
-function make_neighbour_list!(neighbour_list, chain, coord_map)
-    # Remove all elements from list
-    empty!(neighbour_list)
-
-    # Loop over each link in the chain, and then each of the four nearest
-    # neighbours.
-    for (i, coord) in enumerate(chain)
-        for dir in ((1, 0), (-1, 0), (0, 1), (0, -1))
-            neighbour_coord = coord .+ dir
-
-            # get the index in the chain (or 0 if it does not exist)
-            # and then only add a neighbour pair if the neighbour index
-            # is more than one above the current monomer. This means that it
-            # is later in the chain (So that we only store one element per
-            # neighbour pair), and they are not covalently bonded as they
-            # are not adjacent in the chain.
-            neighbour_ind = get(coord_map, neighbour_coord, 0)
-            if neighbour_ind > i + 1
-                push!(neighbour_list, (i, neighbour_ind))
-            end
-        end
-    end
-
-    neighbour_list
-end
-
-# Calculate the total non-covalent energy from the neighbour_list
-function calculate_energy(interaction_matrix, monomer_types, neighbour_list)
-    sum(interaction_matrix[monomer_types[i], monomer_types[j]]
-        for (i, j) in neighbour_list)
-end
-
-# Calculate the total energy directly without having to save the neighbour_list.
-# The code here is almost identical to the make_neighbour_list! function
-# now just adding to the total energy instead of adding elements to the
-# neighbour_list.
-function calculate_energy_direct(interaction_matrix, monomer_types,
-    chain, coord_map)
-
-    energy = 0.0
-
-    # Loop over each link in the chain, and then each of the four nearest
-    # neighbours.
-    for (i, coord) in enumerate(chain)
-        for dir in ((1, 0), (-1, 0), (0, 1), (0, -1))
-            neighbour_coord = coord .+ dir
-
-            # get the index in the chain (or 0 if it does not exist)
-            # and then only add a neighbour pair if the neighbour index
-            # is more than one above the current monomer. This means that it
-            # is later in the chain (So that we only store one element per
-            # neighbour pair), and they are not covalently bonded as they
-            # are not adjacent in the chain.
-            neighbour_ind = get(coord_map, neighbour_coord, 0)
-            if neighbour_ind > i + 1
-                energy += interaction_matrix[monomer_types[i],
-                    monomer_types[neighbour_ind]]
-            end
-        end
-    end
-
-    energy
-end
-
-# Returns a symmetric 20x20 matrix with elements uniformly distributed
-# between -4 and -2
-function make_interaction_energy_matrix()
-    Symmetric([rand() * 2 - 4 for _ in 1:20, _ in 1:20])
-end
+include("lattice2d.jl")
+include("mc2d.jl")
 
 # This is run to "solve" task 2.1.3
 # Here we test a few different foldings of a 15 monomer long protein
 function test_2_1_3()
+    # filepath to save the figures
+    figure_path = "2.1.3-figures"
+
+    # Initialize random interaction energies
     interaction_matrix = make_interaction_energy_matrix()
+    p = Plots.heatmap(Matrix(interaction_matrix)[:, end:-1:1];
+        ratio=1.0, margins=0.2 * Plots.cm)
+    display(p)
+    Plots.savefig(p, joinpath(figure_path, "matrix.pdf"))
+
+    # Choosing a polymer length and initializing the monomer types as random
+    # integers in the range 1:20
     n = 15
     monomer_types = rand(1:20, n)
 
-    chain = make_linear_2d_chain(n)
-    coord_map = make_coord_map(chain)
-    energy = calculate_energy_direct(interaction_matrix, monomer_types,
-        chain, coord_map)
-
-    display(plot_chain(chain, monomer_types))
-
-    @printf "Unfolded energy: %.2f\n" energy
-
-    for w in (10, 7, 5, 3, 2)
+    # Here we test different foldings. The 'width' w indicate how many
+    # monmers the sheet is wide. This means that both 15 and 1 should be
+    # fully unfolded, just along different axes. w = 5 or 3 are special as the
+    # 15 length polymer fits in a perfect 3x5 or 5x3 rectangle, which maximizes
+    # the number of interactions.
+    for w in (10, 7, 5, 3, 2, 1)
         chain = make_folded_2d_chain(n, w)
         coord_map = make_coord_map(chain)
         energy = calculate_energy_direct(interaction_matrix, monomer_types,
             chain, coord_map)
 
-        display(plot_chain(chain, monomer_types))
+        n_interactions = length(make_neighbour_list(chain, coord_map))
+
+        p = plot_chain(chain, monomer_types)
+        Plots.savefig(p, joinpath(figure_path, "$w.pdf"))
 
         @printf "w = %d: %.2f\n" w energy
+        @printf "Number of interactions: %d\n\n" n_interactions
     end
 end
 
+function test_mc2d()
+    interaction_matrix = make_interaction_energy_matrix()
+
+    n = 100
+    monomer_types = rand(1:20, n)
+
+    chain = make_linear_2d_chain(n)
+    coord_map = make_coord_map(chain)
+
+    display(plot_chain(chain, monomer_types))
+
+    energy = calculate_energy_direct(interaction_matrix, monomer_types,
+        chain, coord_map)
+
+    @time for _ in 1:100000
+        energy = do_mc_sweep!(chain, coord_map, interaction_matrix,
+            monomer_types, energy, 10)
+    end
+    display(plot_chain(chain, monomer_types, false))
+end
